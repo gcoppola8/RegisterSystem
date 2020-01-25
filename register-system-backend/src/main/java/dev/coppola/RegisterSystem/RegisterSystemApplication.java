aca44ca447fa14031882d82b3449ac254871d6a1
Gennaro Coppola <gennaro.coppola@outlook.com>
2020-01-25 13:02:12 +0200

    First commit

---
+++ register-system-backend/src/main/java/dev/coppola/RegisterSystem/RegisterSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.coppola.RegisterSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterSystemApplication.class, args);
+	}
+
+}
---
+++ register-system-backend/src/test/java/dev/coppola/RegisterSystem/RegisterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.coppola.RegisterSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
